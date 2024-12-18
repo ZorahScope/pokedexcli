9ff96901c965560369477c1ed3c0fe3c410b1ebf
Zorah <65967638+ZorahScope@users.noreply.github.com>
2024-12-17 20:56:48 -0600

    Add func to process user input for further handling
    Add unit tests for CleanInput()

--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
-func main(){
+func CleanInput(text string) []string {
+	lowerCaseInput := strings.ToLower(text)
+	splitWords := strings.Fields(lowerCaseInput)
+	cleanedInput := make([]string, 0, len(splitWords))
+
+	for _, word := range splitWords {
+		cleanedWord := strings.Trim(word, " ")
+		cleanedInput = append(cleanedInput, cleanedWord)
+	}
+	return cleanedInput
+
+}
+
+func main() {
 	fmt.Println("Hello, World!")
 }
---
+++ repl_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    " weekend warrior          ",
+			expected: []string{"weekend", "warrior"},
+		},
+		{
+			input:    "Grand Burrito",
+			expected: []string{"grand", "burrito"},
+		},
+		{
+			input:    "Cows And Purple muffins",
+			expected: []string{"cows", "and", "purple", "muffins"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := CleanInput(c.input)
+		actualSliceLength := len(actual)
+		expectedSliceLength := len(c.expected)
+		if actualSliceLength != expectedSliceLength {
+			t.Errorf("slice length %v from CleanInput(%#v) != expected %v", actualSliceLength, actual, expectedSliceLength)
+		}
+
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("CleanInput(%v) returned words not matching whats expected", c.input)
+			}
+		}
+	}
+}
